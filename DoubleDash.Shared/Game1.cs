ff07e791aa410445d03ecfa9261e438c9bccd702
cmy4322 <connormyu@gmail.com>
2017-02-09 12:04:32 -0500

    - Block size scale set to 4 - Added Levels 1-4 of World 1 - Commented out walls in Game1 except for initial landing start floor

--- DoubleDash.Shared/Game1.cs
+++ DoubleDash.Shared/Game1.cs
@@ -63,35 +63,37 @@ namespace DoubleDash
             world.AddGameState(MainGame, mainGameTime, MainDraw);
             world.ActivateGameState(MainGame);
 
-            level = LevelReader.Load("Content/level1.json");
+            level = LevelReader.Load("Content/World 1 - Level 1 V2.1.json");
             level.FinishLoading(graphics);
 
             player = new Player(Content.Load<Texture2D>("player"));
             player.position = new Vector2(300, 800);
 
+
+            
             walls = new Walls(graphics);
 
             // start floor
             walls.Create(new Vector2(600, 100), new Vector2(0, 900));
             // jump to other section floor
-            walls.Create(new Vector2(1000, 100), new Vector2(900, 900));
-            // right wall
-            walls.Create(new Vector2(500, 1000), new Vector2(1800, 0));
-            // end block
-            walls.Create(new Vector2(100, 300), new Vector2(1700, 600));
-            // mid block
-            walls.Create(new Vector2(100, 300), new Vector2(1600, 300));
-            // top block
-            walls.Create(new Vector2(100, 300), new Vector2(1700, 0));
-            // top right floor
-            walls.Create(new Vector2(1000, 25), new Vector2(900, 275));
-            // top left floor
-            walls.Create(new Vector2(600, 25), new Vector2(0, 275));
-            // shaft left wall
-            walls.Create(new Vector2(25, 550), new Vector2(1575, 300));
-            // death floor
-            walls.Create(new Vector2(300, 500), new Vector2(600, 1000));
-            walls.Create(new Vector2(25, 550), new Vector2(1425, 300));
+            //walls.Create(new Vector2(1000, 100), new Vector2(900, 900));
+            //// right wall
+            //walls.Create(new Vector2(500, 1000), new Vector2(1800, 0));
+            //// end block
+            //walls.Create(new Vector2(100, 300), new Vector2(1700, 600));
+            //// mid block
+            //walls.Create(new Vector2(100, 300), new Vector2(1600, 300));
+            //// top block
+            //walls.Create(new Vector2(100, 300), new Vector2(1700, 0));
+            //// top right floor
+            //walls.Create(new Vector2(1000, 25), new Vector2(900, 275));
+            //// top left floor
+            //walls.Create(new Vector2(600, 25), new Vector2(0, 275));
+            //// shaft left wall
+            //walls.Create(new Vector2(25, 550), new Vector2(1575, 300));
+            //// death floor
+            //walls.Create(new Vector2(300, 500), new Vector2(600, 1000));
+            //walls.Create(new Vector2(25, 550), new Vector2(1425, 300));
         }
 
         /// <summary>
@@ -153,8 +155,8 @@ namespace DoubleDash
             player.Update(gameTime);
             walls.Update(gameTime);
             player.CheckCollisions(walls.walls);
-            //wall.Update(gameTime);
-            //level.Update(gameTime);
+            walls.Update(gameTime);
+            level.Update(gameTime);
 
             previousKeyboardState = keyboardState;
         }
@@ -177,7 +179,7 @@ namespace DoubleDash
             world.BeginDraw();
             world.Draw(walls.Draw);
             world.Draw(player.Draw);
-            //world.Draw(level.Draw);
+            world.Draw(level.Draw);
             world.EndDraw();
         }
     }
