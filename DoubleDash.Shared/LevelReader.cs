d10594e866693e3e8b3415aa14b88079abe7bd66
Zack <zdhickman@gmail.com>
2017-02-27 12:53:18 -0500

    fix

--- DoubleDash.Shared/LevelReader.cs
+++ DoubleDash.Shared/LevelReader.cs
@@ -13,19 +13,18 @@ namespace DoubleDash
     {
         protected override Level Read(ContentReader input, Level existingInstance)
         {
-            using (var streamReader = new FileStream(filename, FileMode.Open))
+            using (var streamReader = new StreamReader(input.BaseStream))
             {
-                int levelSize = (int)streamReader.Length;
-                Byte[] levelDataRaw = new Byte[levelSize];
-                streamReader.Read(levelDataRaw, 0, (int)levelSize);
-                GameSave save = JsonConvert.DeserializeObject<GameSave>(System.Text.Encoding.Default.GetString(levelDataRaw));
+                JArray a = JArray.Load(new JsonTextReader(streamReader));
                 Level level = new Level();
-                foreach (BlockDescription block in save.blocks)
+                foreach (JObject o in a)
                 {
-                    level.blocksDescription.Add(block);
+                    level.blocksDescription.Add(new BlockDescription(
+                        (int)o["X"],
+                        (int)o["Y"],
+                        (int)o["Width"],
+                        (int)o["Height"]));
                 }
-                level.start = save.start * 4;
-                level.end = save.end * 4;
                 return level;
             }
         }
