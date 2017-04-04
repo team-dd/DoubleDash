3876024f469d8dfe4e86bc6e37bbdae5a3518102
cmy4322 <connormyu@gmail.com>
2017-04-04 16:38:58 -0400

    testing new level with blink puzzle

--- DoubleDash.Shared/Game1.cs
+++ DoubleDash.Shared/Game1.cs
@@ -142,11 +142,14 @@ namespace DoubleDash
 
             //levelManager.AddLevel(LevelReader.Load("Content/Levels/World 2/level1.json"));
 
-            levelManager.AddLevel(LevelReader.Load("content/levels/demo world/demo level 1.json"));
-            levelManager.AddLevel(LevelReader.Load("content/levels/demo world/demo level 2.json"));
-            levelManager.AddLevel(LevelReader.Load("content/levels/demo world/demo level 4.json"));
-            levelManager.AddLevel(LevelReader.Load("content/levels/demo world/demo level 5.json"));
+            //levelManager.AddLevel(LevelReader.Load("content/levels/demo world/demo level 1.json"));
+            //levelManager.AddLevel(LevelReader.Load("content/levels/demo world/demo level 2.json"));
+            //levelManager.AddLevel(LevelReader.Load("content/levels/demo world/demo level 4.json"));
+            //levelManager.AddLevel(LevelReader.Load("content/levels/demo world/demo level 5.json"));
             levelManager.AddLevel(LevelReader.Load("content/levels/demo world/demo level 7.json"));
+
+
+            //levelManager.AddLevel(LevelReader.Load("content/levels/test Levels/testlevevl9.json"));
 
 
             //levelManager.AddLevel(LevelReader.Load("Content/Levels/Test Levels/zacktry1.json"));
