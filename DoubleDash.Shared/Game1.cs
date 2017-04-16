e54aa59bf43d8bae66bda1d403e548a29f49d5ea
cmy4322 <connormyu@gmail.com>
2017-04-16 13:34:02 -0400

    World 2

--- DoubleDash.Shared/Game1.cs
+++ DoubleDash.Shared/Game1.cs
@@ -197,13 +197,20 @@ namespace DoubleDash
             levelManager = new LevelManager(World.TextureManager["door"], graphics, doorSound);
 
             //World 1
-            levelManager.AddLevel(LevelReader.Load("Content/Levels/World 1/level1.json"));
-            levelManager.AddLevel(LevelReader.Load("Content/levels/World 1/level2.json"));
-            levelManager.AddLevel(LevelReader.Load("Content/levels/World 1/level3.json"));
-            levelManager.AddLevel(LevelReader.Load("Content/levels/World 1/level4.json"));
-            levelManager.AddLevel(LevelReader.Load("Content/levels/World 1/level5.json"));
-            levelManager.AddLevel(LevelReader.Load("Content/levels/World 1/level6.json"));
-            levelManager.AddLevel(LevelReader.Load("Content/levels/World 1/puzzle1.json"));
+            //levelManager.AddLevel(LevelReader.Load("Content/Levels/World 1/level1.json"));
+            //levelManager.AddLevel(LevelReader.Load("Content/levels/World 1/level2.json"));
+            //levelManager.AddLevel(LevelReader.Load("Content/levels/World 1/level3.json"));
+            //levelManager.AddLevel(LevelReader.Load("Content/levels/World 1/level4.json"));
+            //levelManager.AddLevel(LevelReader.Load("Content/levels/World 1/level5.json"));
+            //levelManager.AddLevel(LevelReader.Load("Content/levels/World 1/level6.json"));
+            //levelManager.AddLevel(LevelReader.Load("Content/levels/World 1/puzzle1.json"));
+
+            //World 2
+            //levelManager.AddLevel(LevelReader.Load("Content/levels/World 2/level1.json"));
+            //levelManager.AddLevel(LevelReader.Load("Content/levels/World 2/level2.json"));
+            //levelManager.AddLevel(LevelReader.Load("Content/levels/World 2/level3.json"));
+            levelManager.AddLevel(LevelReader.Load("Content/levels/World 2/level4.json"));
+            //levelManager.AddLevel(LevelReader.Load("Content/levels/World 2/level6.json"));
 
 
             //levelManager.AddLevel(LevelReader.Load("content/levels/demo world/demo level 1.json"));
